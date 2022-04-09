b3bdba2bf4f68c8b1c772b785fed0a5cd5ad394e
Juan Pedro Tovar <jptovar92@gmail.com>
2022-04-09 16:22:42 -0400

    Port Correction

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 require("dotenv").config();
 app.use(cookieParser());
 // * Puerto para heroku
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 // * Puerto para heroku
 
 app.listen(port, () => {
